5a31e97ad2b31a51bda436173864d973fa0c9159
Peter Holt <peter.holt@dochq.co.uk>
2019-02-21 10:30:58 +0000

    go_vet lint

--- ResultBulk.go
+++ ResultBulk.go
@@ -6,7 +6,7 @@ package postcodesio
 type ResultsBulk struct {
 	Status int `json:"status,omitempty"`
 	Result []struct {
-		Query  string   `json:"query, omitempty"`
+		Query  string   `json:"query,omitempty"`
 		Result []Result `json:"result,omitempty"`
 	} `json:"result, omitempty"`
 }
